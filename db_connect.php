961f461c216fdc5b3ecffaf57d9889ce8daa4660
KoEonYack <21300035@handong.edu>
2018-05-13 22:47:14 +0900

    fix typo

--- db_connect.php
+++ db_connect.php
@@ -2,15 +2,14 @@
     #=======================================================================
     $db_host = "localhost";
     $db_user = "root";
-    $db_passwd = "est2678s";
+    $db_passwd = "*****";
     $db_name = "db_project";
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
 
     if (mysqli_connect_errno($conn)){
         echo "Database connection failed: ".mysqli_connect_error();
     }else{
-        echo "connect DB_1     <br>";
+        echo "connect DB <br>";
     }
     #=======================================================================
-
 ?>
