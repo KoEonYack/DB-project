203563862a015ffb1419faae95564b0dd8b91a13
KoEonYack <21300035@handong.edu>
2018-05-13 21:56:56 +0900

    add db_connect.php

--- sql5.php
+++ sql5.php
@@ -15,19 +15,9 @@
         <h2>SQL Table</h2>
         <p>SELECT * FROM joyful_trop WHERE board </p>            
         <?php
-            $db_host = "localhost";
-            $db_user = "root";
-            $db_passwd = "****";
-            $db_name = "joyful_trip";
-            $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
 
-            if (mysqli_connect_errno($conn)){
-                echo "Database connection failed: ".mysqli_connect_error();
-            }else{
-                echo ". <br>";
-            }
-
-            $result = mysqli_query($conn, "SELECT * FROM board ");
+            require('db_connect.php');
+            $result = mysqli_query($conn, "SELECT * FROM movie_list ");
 
            echo "<table width='100%' class='table table-striped table-bordered table-hover' id='dataTables-example' >";
            # echo "<table class='table table-hover' >";
@@ -35,9 +25,9 @@
             echo "<tr><th style='text-align:center;'>요청자 ID</th><th style='text-align:center;'>글 제목</th><th style='text-align:center;'>요청 내용</th></tr>";
 
             while( $row = mysqli_fetch_array($result) ){  
-                $id = $row["name"];
-                $title = $row["title"];
-                $content = $row["content"];
+                $id = $row["movie_id"]; 
+                $title = $row["movie_name"];
+                $content = $row["english_name"];
 
                 #echo "<tr><td><div>".$id."</div></td><td>".$title."</td><td  style='padding-right:200px;'>".$content."</td></tr>";
                 echo "<tr>  <td>".$id."</td>  <td>".$title."</td>  <td>".$content."</td>   </tr>";
@@ -47,7 +37,7 @@
             mysqli_close($conn);
         ?>
         
-        <a href="front.html"><button type="button" class="btn btn-primary">Go To Main</button></a>
+        <a href="button.html"><button type="button" class="btn btn-primary">Go To Main</button></a>
         </div>
     </body>
 </html>
