c14a26f4ec8c8bb1ba26e57c1b5d8a36ff5a9cc2
KoEonYack <21300035@handong.edu>
2018-05-15 08:53:39 +0900

    modify design actor_info

--- HRB_project/actor_info_query.php
+++ HRB_project/actor_info_query.php
@@ -4,12 +4,14 @@
       if(isset($_GET['id'])){
         global $conn;
         $sql = "SELECT * FROM actor_list WHERE actor_id=".$_GET['id'];
-        echo '<h1>'.$sql.'</h1>';
+        echo "<hr>";
+        echo '<h4>'.$sql.'</h4>';
         $result = mysqli_query($conn,$sql);
         $row = mysqli_fetch_array($result);
-        echo '<br><span style="font-size:35px">나이 : '.$row['actor_age'].'</span>';
-        echo '<br><span style="font-size:35px">성별 : '.$row['actor_gender'].'</span>';
-        echo '<br><span style="font-size:35px">역할 : '.$row['actor_type'].'</span><br>';
+
+        echo '<h5>나이 : '.$row['actor_age'].'</h5>';
+        echo '<h5>성별 : '.$row['actor_gender'].'</h5>';
+        echo '<h5>역할 : '.$row['actor_type'].'</h5>';
         echo '<br><img src="'.$row['photo_url'].'">';
       }
     }
@@ -20,12 +22,15 @@
       echo '<h1>'.$sql.'</h1>';
       $result = mysqli_query($conn, $sql);
       $n=0;
+      echo "<p>";
       while($row = mysqli_fetch_array($result)){
+        
         echo '<button type="button" class="btn btn-default"><a href="actor_info.php?id='
         .$row['actor_id'].'" style="text-decoration:none; color:black;">'.$row['actor_name'].'</a></button>';
         echo '  ';
         $n++;
-        if($n % 5 === 0) echo '<br>';
+        if($n%10===0) echo '</p><p>';
       }
+      
     }
 ?>
