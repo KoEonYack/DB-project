1fc1338bdcbaea6eed0bc560e0b9fc971ca66809
KoEonYack <21300035@handong.edu>
2018-05-15 09:21:31 +0900

    modify design movie_genre page

--- HRB_project/best_comment_query.php
+++ HRB_project/best_comment_query.php
@@ -6,7 +6,7 @@
         $user_id=1;
         // 먼저 해당 유저가 쓴 comment_id를 전부 가져온다.
         $comment_sql = "SELECT comment_id FROM comment_list WHERE userlist_id={$user_id};";
-        echo '<h1>'.$comment_sql.'</h1>';
+        echo '<h4>'.$comment_sql.'</h4>';
         $comment_result = mysqli_query($conn, $comment_sql);
 
         // 총 좋아요 개수와 최고 코멘트를 알아내기 위한 연관 배열 선언
@@ -18,7 +18,7 @@
             $like_sql =
             "SELECT COUNT(comment_id) AS like_num FROM comment_like_list
             WHERE comment_id={$comment['comment_id']};";
-            echo '<h1>'.$like_sql.'</h1>';
+            echo '<h4>'.$like_sql.'</h4>';
             $like_result = mysqli_query($conn, $like_sql);
             $like = mysqli_fetch_array($like_result);
 
@@ -31,13 +31,13 @@
         // 가장 like_num이 많은 comment_id를 가져온다.
         $best_comment_id = $best_comment_list[max($like_num_list)];
         $contents_sql = "SELECT contents FROM comment_list WHERE comment_id={$best_comment_id};";
-        echo '<h1>'.$contents_sql.'</h1>';
+        echo '<h4>'.$contents_sql.'</h4>';
         $contents_result = mysqli_query($conn, $contents_sql);
         $contents = mysqli_fetch_array($contents_result);
 
-        echo '<br><span style="font-size:35px">
+        echo '<br><span style="font-size:20px">
         최고 코멘트 : '.$contents['contents'].'</span>';
-        echo '<br><span style="font-size:35px">
+        echo '<br><span style="font-size:20px">
         이제까지 받은 총 좋아요 수 : '.array_sum($like_num_list).'</span>';
     }
 ?>
