8067b8b07e71f2fa4575b8926588206ed4e55349
bae haram <gkfkagkfka12@naver.com>
2018-05-15 09:27:39 +0900

    Add actor's main movie, name and modify some invalid data

--- HRB_project/actor_info_query.php
+++ HRB_project/actor_info_query.php
@@ -3,16 +3,26 @@
     function actorClick(){
       if(isset($_GET['id'])){
         global $conn;
-        $sql = "SELECT * FROM actor_list WHERE actor_id=".$_GET['id'];
+        $actor_sql = "SELECT * FROM actor_list WHERE actor_id=".$_GET['id'];
         echo "<hr>";
         echo '<h4>'.$sql.'</h4>';
-        $result = mysqli_query($conn,$sql);
-        $row = mysqli_fetch_array($result);
+        $result = mysqli_query($conn,$actor_sql);
+        $actor = mysqli_fetch_array($result);
 
-        echo '<h5>나이 : '.$row['actor_age'].'</h5>';
-        echo '<h5>성별 : '.$row['actor_gender'].'</h5>';
-        echo '<h5>역할 : '.$row['actor_type'].'</h5>';
-        echo '<br><img src="'.$row['photo_url'].'">';
+        $movie_sql = 
+        "SELECT m.movie_name FROM movie_list m 
+        LEFT JOIN movie_actor_list ma ON m.movie_id=ma.movie_id
+        WHERE ma.actor_id=".$actor['actor_id'];
+
+        $result = mysqli_query($conn, $movie_sql);
+        $movie = mysqli_fetch_array($result);
+
+        echo '<h5>이름 : '.$actor['actor_name'].'</h5>';
+        echo '<h5>나이 : '.$actor['actor_age'].'</h5>';
+        echo '<h5>성별 : '.$actor['actor_gender'].'</h5>';
+        echo '<h5>역할 : '.$actor['actor_type'].'</h5>';
+        echo '<h5>출연 : '.$movie['movie_name'].'</h5>';
+        echo '<br><img src="'.$actor['photo_url'].'" width="200px"  >';
       }
     }
 
@@ -23,10 +33,10 @@
       $result = mysqli_query($conn, $sql);
       $n=0;
       echo "<p>";
-      while($row = mysqli_fetch_array($result)){
+      while($actor = mysqli_fetch_array($result)){
         
         echo '<button type="button" class="btn btn-default"><a href="actor_info.php?id='
-        .$row['actor_id'].'" style="text-decoration:none; color:black;">'.$row['actor_name'].'</a></button>';
+        .$actor['actor_id'].'" style="text-decoration:none; color:black;">'.$actor['actor_name'].'</a></button>';
         echo '  ';
         $n++;
         if($n%10===0) echo '</p><p>';
