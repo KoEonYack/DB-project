63dbd9b86085683b8f4afb3dffcfb4ecda2b397c
bae haram <gkfkagkfka12@naver.com>
2018-05-15 20:40:57 +0900

    Change design

--- HRB_project/director_info_query.php
+++ HRB_project/director_info_query.php
@@ -6,11 +6,20 @@
         $sql = "SELECT * FROM director_list WHERE director_id=".$_GET['id'];
         echo '<h4>'.$sql.'</h4><hr><br>';
         $result = mysqli_query($conn,$sql);
-        $row = mysqli_fetch_array($result);
+        $director = mysqli_fetch_array($result);
 
-        echo '<h5>이름 : '.$row['director_name'].'</h5>';
-        echo '<h5>나이 : '.$row['director_age'].'</h5>';
-        echo '<br><img src="'.$row['photo_url'].'">';
+        $movie_sql = 
+        "SELECT m.movie_name FROM movie_list m 
+        LEFT JOIN movie_director_list md ON m.movie_id=md.movie_id
+        WHERE md.director_id=".$_GET['id'];
+
+        $result = mysqli_query($conn, $movie_sql);
+        $movie = mysqli_fetch_array($result);
+
+        echo '<h5>이름 : '.$director['director_name'].'</h5>';
+        echo '<h5>나이 : '.$director['director_age'].'</h5>';
+        echo '<h5>작품 : '.$movie['movie_name'].'</h5>';
+        echo '<br><img src="'.$director['photo_url'].'" width="200px">';
       }
     }
 
@@ -21,9 +30,9 @@
       $result = mysqli_query($conn, $sql);
       $n=0;
       echo "<p>";
-      while($row = mysqli_fetch_array($result)){
+      while($director = mysqli_fetch_array($result)){
         echo '<button type="button" class="btn btn-default"><a href="director_info.php?id='
-        .$row['director_id'].'" style="text-decoration:none; color:black;">'.$row['director_name'].'</a></button>';
+        .$director['director_id'].'" style="text-decoration:none; color:black;">'.$director['director_name'].'</a></button>';
         echo '  ';
         $n++;
         if($n%8===0) echo '</p><p>';
--- HRB_project/movie_nation_query.php
+++ HRB_project/movie_nation_query.php
@@ -1,23 +1,19 @@
 <?php
-    require('/../db_connect.php');
+    require('../db_connect.php');
     // 본 영화리스트 출력하는 함수
     function movieSawNationList(){
       global $conn;
-
       echo '<br><span style="font-size:35px"><국가별로 보기></span><br>';
       $nation_sql = "SELECT DISTINCT nation FROM movie_list";
-      echo '<h1>'.$nation_sql.'</h1>';
+      echo '<h4>'.$nation_sql.'</h4><hr>';
       $nation_result = mysqli_query($conn, $nation_sql);
-      $n = 0;
       while($nation = mysqli_fetch_array($nation_result)){
-        echo '<button type="button"><span style="font-size:20px"><a href="movie_nation.php?id='
+        echo '<button type="button" class="btn btn-default"><a href="movie_nation.php?id='
         .$nation["nation"].'"style="text-decoration:none; color:black;">'
-        .$nation["nation"].'</a></span></button>&nbsp;&nbsp;&nbsp;';
+        .$nation["nation"].'</a></button>';
+        echo '  ';
         $n++;
-        if($n%5===0) echo '<br><br>';
       }
-
-      echo "<br>-------------------------------------------------------------<br>";
     }
 
     function nationClick(){
@@ -29,13 +25,14 @@
         LEFT JOIN user_rating_list u
         ON m.movie_id=u.movie_id
         WHERE u.userlist_id={$user_id} AND m.nation='{$_GET['id']}';";
-        echo '<h1>'.$movie_nation_sql.'</h1>';
+        echo '<h4>'.$movie_nation_sql.'</h4>';
 
         $result = mysqli_query($conn, $movie_nation_sql);
+        echo '<div class="list-group" style="display:inline-block;">';
         while($movie_nation=mysqli_fetch_array($result)){
-          echo '<br><span style="font-size:35px"><a href="movie_saw.php?id='
-          .$movie_nation['movie_id'].'"style="text-decoration:none; color:black;">'
-          .$movie_nation['movie_name'].'</a></span><br>';
+          echo '<a href="../KEY_project/movie_info.php?id='
+          .$movie_nation["movie_id"].'" class="list-group-item list-group-item-action" style="text-decoration:none; color:black;">'
+          .$movie_nation["movie_name"].'</a>';  
         }
 
       }
