1352607bd4c747a200aad7af7d07aea333186e52
bae haram <gkfkagkfka12@naver.com>
2018-05-24 23:48:59 +0900

    Add award, genre, tag, actor

--- KEY_project/movie_info_quary.php
+++ KEY_project/movie_info_quary.php
@@ -11,23 +11,7 @@
             INNER JOIN award_list AS AW ON MV.award_id = AW.award_id
             WHERE movie_id="'.$_GET['id']';
  */
-            $sql = "SELECT MV.movie_name, MV.english_name,
-            MV.release_date ,
-            MV.nation ,
-            MV.site_url ,
-            MV.budget ,
-            MV.revenue ,
-            MV.language ,
-            MV.rating_certification ,
-            MV.advance_rate ,
-            MV.studio_name,
-            MV.cumulative_audience,
-            MV.author,
-            MV.running_time,
-            MV.summary,
-            AW.award_name FROM movie_list AS MV 
-            INNER JOIN award_list AS AW ON MV.award_id = AW.award_id
-            WHERE movie_id=".$_GET['id'];
+            $sql = "SELECT * FROM movie_list WHERE movie_id=".$_GET['id'];
 
             echo '<h4>'.$sql.'</h4>';
             $result = mysqli_query($conn,$sql);
@@ -46,8 +30,43 @@
             echo '<h5>예매율 : '.$row['advance_rate'].'</h5>';
             echo '<h5>제작사 : '.$row['studio_name'].'</h5>';
             echo '<h5>작가 : '.$row['author'].'</h5>';
-            echo '<p>영화 상세 설명 : '.$row['summary'].'</p>';       
-            echo '<h5>수상작 : '.$row['award_name'].'</h5>'; 
+            echo '<p>영화 상세 설명 : '.$row['summary'].'</p>'; 
+            
+            $sql = "SELECT award_name FROM award_list a 
+            INNER JOIN (SELECT award_id FROM movie_award_list WHERE movie_id=".$_GET['id'].") AS ma ON a.award_id=ma.award_id";
+            echo '<h5>'.$sql.'<h5>';
+            $result = mysqli_query($conn, $sql);
+            echo '<h5>수상작 : ';
+            while($row = mysqli_fetch_array($result))
+                echo $row['award_name'].' | ';
+            echo '</h5>';
+
+            $sql = "SELECT tag_name FROM tag_list t 
+            INNER JOIN (SELECT tag_id FROM movie_tag_list WHERE movie_id=".$_GET['id'].") AS mt ON t.tag_id=mt.tag_id";
+            echo '<h5>'.$sql.'<h5>';
+            $result = mysqli_query($conn, $sql);
+            echo '<h5>태그 : ';
+            while($row = mysqli_fetch_array($result))
+                echo $row['tag_name'].' | ';
+            echo '</h5>';
+
+            $sql = "SELECT genre_name FROM genre_list g 
+            INNER JOIN (SELECT genre_id FROM movie_genre_list WHERE movie_id=".$_GET['id'].") AS mg ON g.genre_id=mg.genre_id";
+            echo '<h5>'.$sql.'<h5>';
+            $result = mysqli_query($conn, $sql);
+            echo '<h5>장르 : ';
+            while($row = mysqli_fetch_array($result))
+                echo $row['genre_name'].' | ';
+            echo '</h5>';
+
+            $sql = "SELECT actor_name FROM actor_list ac 
+            INNER JOIN (SELECT actor_id FROM movie_actor_list WHERE movie_id=".$_GET['id'].") AS mac ON ac.actor_id=mac.actor_id";
+            echo '<h5>'.$sql.'<h5>';
+            $result = mysqli_query($conn, $sql);
+            echo '<h5>출연배우 : ';
+            while($row = mysqli_fetch_array($result))
+                echo $row['actor_name'].' | ';
+            echo '</h5>';
         }
           else{
               echo "Error :: invaild id";
