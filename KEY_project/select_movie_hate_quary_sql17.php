01d22c8d36fe1c0f9e8a299cfda8a7c9cc850935
bae haram <gkfkagkfka12@naver.com>
2018-05-25 22:38:05 +0900

    Delete error,sql message and modify printing message

--- KEY_project/select_movie_hate_quary_sql17.php
+++ KEY_project/select_movie_hate_quary_sql17.php
@@ -1,26 +1,25 @@
 <?php
     function movieClick(){
         if(isset($_GET['id'])){
-            echo $_GET['id'];
             
             global $conn;
             
             require('../db_connect.php');
-            $sql = "INSERT INTO user_hate_list VALUES (".$_GET['id'].",".$_SESSION['userlist_id'].")";
+            $sql = "INSERT INTO user_hate_list VALUES (".$_SESSION['userlist_id'].",".$_GET['id'].")";
             $result = mysqli_query($conn, $sql);
 
             $sql1 = "SELECT movie_name FROM movie_list WHERE movie_id=".$_GET['id'];
             echo '<h4>'.$sql.'</h4>';
             $result = mysqli_query($conn, $sql1);
             $row = mysqli_fetch_array($result);
-            echo "<br><h5> 선택한 싫어하는 영화 이름  : ".$row['movie_name']."<h5>";
+            echo '<h5>'.$row["movie_name"].'을(를) 관심없어요에 담았습니다.</h5>';
 
             // echo '<h5>유저 ID : '.$row['userlist_id'].'</h5>';
             // echo '<h5>개봉국가 : '.$row['nation'].'</h5>';
 
         }
           else{
-              echo "Error :: invaild id";
+              //echo "Error :: invaild id";
           }
         }
         
