d1331b9e3c4553b613497ce20688c518266c811b
eunsung <gldghd5662@gmail.com>
2018-05-26 18:21:38 +0900

    user_id session을 통해 받아오는 값으로 반영

--- ESK_project/user_prefrence_tag_query.php
+++ ESK_project/user_prefrence_tag_query.php
@@ -10,7 +10,7 @@
 
     $tag_sql = "SELECT tag_name FROM tag_list JOIN 
     (SELECT tag_id FROM user_rating_list JOIN movie_tag_list ON user_rating_list.movie_id=movie_tag_list.movie_id 
-    WHERE user_rating_list.userlist_id=1 AND user_rating_list.star_rate>=4) tag_num ON tag_num.tag_id= tag_list.tag_id;";
+    WHERE user_rating_list.userlist_id={$user_id} AND user_rating_list.star_rate>=4) tag_num ON tag_num.tag_id= tag_list.tag_id;";
 
     echo '<br><h1>'.$user_name. ' 유저의 선호 태그 입니다. 평점 4.0 이상 준 영화를 기준으로 작성되었습니다.  </h1><p>';
     echo '<h4>'.$tag_sql.'</h4>';
