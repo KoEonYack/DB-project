cb2c0cb326afabf75293ea57df6e93a5b2908200
bae haram <gkfkagkfka12@naver.com>
2018-05-26 21:26:58 +0900

    Add function to view log of login

--- Admin/admin.php
+++ Admin/admin.php
@@ -83,7 +83,8 @@
 
         <p>
             <a href="./admin_addActor.php"><button type="button" class="btn btn-primary">영화에 배우 추가하기(TRANSACTION)</button></a>
-            <a href="./admin_loginlog.php"><button type="button" class="btn btn-primary">유저의 회원가입 로그확인(TRIGGER)</button></a>
+            <a href="./admin_signUPlog.php"><button type="button" class="btn btn-primary">유저의 회원가입 로그확인(TRIGGER)</button></a>
+            <a href="./admin_loginlog.php"><button type="button" class="btn btn-primary">유저의 로그인 로그확인</button></a>
         </p>
         
         <br><br><br><br><br>
