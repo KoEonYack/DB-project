5c33a18c80d91b1c8c26ea7a56d1927f0de9a1a1
bae haram <gkfkagkfka12@naver.com>
2018-05-26 21:11:03 +0900

    Add query to each function

--- Admin/admin.php
+++ Admin/admin.php
@@ -78,12 +78,12 @@
 
         <p>
             <a href="../MCK_project/makeMovie_DECREASEINDEX.php"><button type="button" class="btn btn-primary"> Decreasing order INDEX를 movie_list에 적용하기 (VIEW)</button></a>
-            <a href="../MCK_project/updateUserPassWord.php"><button type="button" class="btn btn-primary"> 미소지기님의 비밀번호를 \'명철123\'으로 변경하기 (UPDATE)</button></a>
-            <a href="./admin_addActor.php"><button type="button" class="btn btn-primary">영화에 배우 추가하기</button></a>
+            <a href="../MCK_project/updateUserPassWord.php"><button type="button" class="btn btn-primary"> 미소지기님의 비밀번호를 '명철123'으로 변경하기 (UPDATE)</button></a>
+            <a href="./admin_addActor.php"><button type="button" class="btn btn-primary">영화에 배우 추가하기(TRANSACTION)</button></a>
         </p>
 
         <p>
-            <a href="./admin_loginlog.php"><button type="button" class="btn btn-primary">유저의 회원가입 로그확인</button></a>
+            <a href="./admin_loginlog.php"><button type="button" class="btn btn-primary">유저의 회원가입 로그확인(TRIGGER)</button></a>
         </p>
         
         <br><br><br><br><br>
