4f2f02091426824a734d6e211028354498d1aec7
bae haram <gkfkagkfka12@naver.com>
2018-05-26 09:12:48 +0900

    Add constraint for only login

--- HRB_project/star_info_query.php
+++ HRB_project/star_info_query.php
@@ -3,7 +3,7 @@
 
     function starAverage(){
         global $conn;
-        $user_id=1;
+        $user_id=$_SESSION['userlist_id'];
         $sql = "SELECT AVG(star_rate) AS star_avg FROM user_rating_list WHERE userlist_id=".$user_id;
         echo '<h4>'.$sql.'</h4><p>';
         $result = mysqli_query($conn,$sql);
@@ -17,7 +17,7 @@
     
     function frequentStar(){
         global $conn;
-        $user_id=1;
+        $user_id=$_SESSION['userlist_id'];
         $sql = 
         "SELECT star_rate, COUNT(star_rate) AS frequency FROM user_rating_list WHERE userlist_id={$user_id}
          GROUP BY star_rate ORDER BY frequency DESC LIMIT 1";
